8a8ce9151bf57e0fa1108401567c20f97e2e9ab4
Christer Eriksson <christer@dogtail.se>
2023-02-28 17:15:32 +0100

    Add UnitTest project

---
+++ Vmr.Dependency1.UnitTests/CalculatorTests.cs
@@ -0,0 +1,24 @@
+using Vrm.Dependency1;
+
+namespace Vmr.Dependency1.UnitTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Calculator_Add()
+        {
+            // Arrange
+            var arg1 = 1;
+            var arg2 = 2;
+            var expectedResult = 3;
+
+            // Act
+            var calculator = new Calculator();
+            var result = calculator.Add(arg1, arg2);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ Vrm.Dependency1/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace Vrm.Dependency1
+{
+    public class Calculator
+    {
+        public int Add(int arg1, int arg2)
+        {
+            return arg1 + arg2;
+        }
+
+        public int Subtract(int arg1, int arg2)
+        {
+            return arg1 - arg2;
+        }
+
+    }
+}
